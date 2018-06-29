ea9a04184a07ebb747b9cf60cf494cd3563b71c8
roxasdeng <332519096@qq.com>
2018-06-29 17:31:58 +0800

    1731

---
+++ js/button.js
@@ -0,0 +1,29 @@
+{
+    let view = {
+        el:'footer > .button',
+        init(){
+            this.$el = $(this.el);
+        },
+        active(e){
+            let $button = $(e.currentTarget);
+            $button.addClass('active').siblings().removeClass('active');
+            var index = $button.index();
+            $('section').removeClass('active').eq(index).addClass('active')
+        }
+    }
+    let model = {}
+    let controller = {
+        init(view,model){
+            this.view = view;
+            this.model = model;
+            this.view.init();
+            this.bindEvent();
+        },
+        bindEvent(){
+            this.view.$el.on('click',(e)=>{
+                this.view.active(e);
+            })
+        }
+    }
+    controller.init(view,model);
+}
